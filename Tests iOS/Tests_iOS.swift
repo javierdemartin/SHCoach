24678d6c3c2ad2c3cbe545774a883daf061cd313
Javier de Martín <javierdemartin@gmail.com>
2021-09-03 10:52:17 +0200

    Deleted unused code and removed force unwraps in favor of XCTUnwrap

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -23,6 +23,9 @@ fileprivate extension AVURLAsset {
 /**
  Test all the functionality of both `SHCatalogCreator` & `SHCatalogInstance` capabilities of creating a
  custom `ShazamKit` catalog and audio matching.
+ 
+ Both `SHCatalogCreator` & `SHCatalogMatcher` in the test use a custom mocked instance of
+ `AVAudioEngine`, `FakeMicrophone`, as it's not possible to create a "real" hardware instance of a microphone.
  */
 class Tests_iOS: XCTestCase {
     
@@ -55,19 +58,25 @@ class Tests_iOS: XCTestCase {
             fatalError("Could not find file.")
         }
         
-        let correctSignature = creator.generateSignature(from: audioFilePath)!
+        let correctSignature = try XCTUnwrap(creator.generateSignature(from: audioFilePath), "Failed to create signature")
         
-        creator.customSignatures.append(SHCoachCustomSignatureModel(signature: correctSignature, mediaItemProperties: [.title: correctLongSong.fileNameWithoutExtension]))
+        creator.customSignatures.append(
+            SHCoachCustomSignatureModel(
+                signature: correctSignature,
+                mediaItemProperties: [.title: correctLongSong.fileNameWithoutExtension])
+        )
         
-        let catalog = creator.createCustomCatalog(with: creator.customSignatures)!
+        let catalog = try XCTUnwrap(creator.createCustomCatalog(with: creator.customSignatures), "Failed to create custom catalog")
         
         matcher.loadModel(model: catalog)
         
-        let wrongSignature = creator.generateSignature(from: wrongAudioToMatch)!
+        let wrongSignature = try XCTUnwrap(creator.generateSignature(from: wrongAudioToMatch), "Failed to create signature")
+        
+        // Match a wrong song that does not exist in the custom catalog
+        // to trigger a failed state
         matcher.match(signature: wrongSignature)
         
-        
-        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 5.0, execute: {
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 2.0, execute: {
             
             if case .matchFound = matcher.state {
                 fatalError("A match shouldn't have been made as both songs are different.")
@@ -76,7 +85,7 @@ class Tests_iOS: XCTestCase {
             }
         })
         
-        wait(for: [expectation], timeout: 10.0)
+        wait(for: [expectation], timeout: 5.0)
     }
     
     /// Create a `SHCustomCatalog` with a full-length audio. And try to match that song
@@ -108,24 +117,22 @@ class Tests_iOS: XCTestCase {
         
         creator.customSignatures.append(customSignatureModel)
         
-        let catalog = creator.createCustomCatalog(with: creator.customSignatures)!
+        let catalog = try XCTUnwrap(creator.createCustomCatalog(with: creator.customSignatures), "Failed to create custom catalog")
         
         matcher.loadModel(model: catalog)
         
-        
         matcher.match(signature: signatureToMatch)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
             
             if case .matchFound = matcher.state {
                 expectation.fulfill()
-                
             } else {
                 fatalError("Song should've been matched correctly")
             }
         })
         
-        wait(for: [expectation], timeout: 10.0)
+        wait(for: [expectation], timeout: 5.0)
     }
     
     /// Read an audio file and
@@ -133,8 +140,6 @@ class Tests_iOS: XCTestCase {
     /// omitted in the original song.
     ///
     func testCreateSignatureFromAudioFile() throws {
-        
-        let expectation = XCTestExpectation(description: "Creating a sample signature from an audio file.")
         
         guard let audioFilePath = Bundle(for: type(of: self)).url(forResource: correctLongSong.fileNameWithoutExtension, withExtension: correctLongSong.pathExtension) else {
             fatalError("Could not find 'Sample.pdf' file to test.")
@@ -154,16 +159,13 @@ class Tests_iOS: XCTestCase {
         // as they don't contain any information relevant to the signature.
         XCTAssertTrue(Int(unwrappedSignature.duration) <= Int(duration), "Signature's length should be equal or smaller than the original audio duration")
         XCTAssertTrue(unwrappedSignature.dataRepresentation.isEmpty == false, "Signature's data is empty")
-        
-        expectation.fulfill()
     }
     
-    /// Read a long song, create a custom catalog and export it.
+    /// Read a full length song, create a custom catalog and export it.
     /// Check if any error happens in the process and if the returned URL where the catalog has been stored in is valid.
     func testCreateCustomCatalogAndExport() throws {
         
-        let expectation = XCTestExpectation(description: "Creating a sample signature from an audio file.")
-        
+        // Read a full length song which will be used to create the `SHCustomCatalog`
         guard let nonAudioFilePath = Bundle(for: type(of: self)).url(forResource: correctLongSong.fileNameWithoutExtension, withExtension: correctLongSong.pathExtension) else {
             fatalError("Could not find \(correctLongSong) in project.")
         }
@@ -185,7 +187,5 @@ class Tests_iOS: XCTestCase {
         let exportedCustomCatalogUrl = try XCTUnwrap(creator.export(customCatalog))
         
         XCTAssertTrue(exportedCustomCatalogUrl.isFileURL)
-        
-        expectation.fulfill()
     }
 }
